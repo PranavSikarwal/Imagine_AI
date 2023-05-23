ee901814744871c11eb826693483926423556702
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-05-23 19:23:47 +0530

    add rootDir to view

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(_req,res,_next)=>{
-    res.render('home.ejs',{path: '/'}); // passed path to track active <a> class in header.ejs
+    res.render('home',{path: '/'}); // passed path to track active <a> class in header.ejs
 });
 
 router.get('/generateImg',(_req,res,_next)=>{
-    res.render('imagineAI.ejs',{path: '/generateImg'});
+    res.render('imagineAI',{path: '/generateImg'});
 });
 
 module.exports = router;
