0422589cdaec2d06e8df86fc6f48fbd79e9d7957
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-05-23 18:19:00 +0530

    add .ejs to home path

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(_req,res,_next)=>{
-    res.render('home',{path: '/'}); // passed path to track active <a> class in header.ejs
+    res.render('home.ejs',{path: '/'}); // passed path to track active <a> class in header.ejs
 });
 
 router.get('/generateImg',(_req,res,_next)=>{
-    res.render('imagineAI',{path: '/generateImg'});
+    res.render('imagineAI.ejs',{path: '/generateImg'});
 });
 
 module.exports = router;
